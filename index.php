28a0ad72f62db6fe8597e2f52449df733b6d7c62
StevenAEN <s.barbe@groupe-aen.info>
2023-03-22 12:06:12 +0100

    Je test

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="icon" type="image/png" href="./images/favicon/favicon-32x32.png" />
     <title>AGEN ESCAPE - ACCUEIL</title>
+    <!-- TEST COMMENTAIRE -->
   </head>
   <body>
     <div id="messageFormulaire" class="overlay">
