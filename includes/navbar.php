951e9d60e57045eab007e1a1288b869e411708ff
StevenAEN <s.barbe@groupe-aen.info>
2023-03-22 11:42:33 +0100

    Mise a jour includes

---
+++ includes/navbar.php
@@ -0,0 +1,14 @@
+<div class="container_nav">
+        <div id="div_nav" style="border-radius: 20px;">
+          <div id="div-ul">
+            <ul id="links_menu">
+
+            <li><a href="./index.php#scenariosList">SCENARIOS</a></li><li><a href="./tarif.php">TARIFS</a></li><li><a href="./reservation.php">RESERVER</a></li><li><a href="./index.php#nousTrouver">NOUS TROUVER</a></li></ul>
+          </div>
+          <div class="icons"><a href="https://www.youtube.com/watch?v=sxzRYuYH2Xc" target="_blank"><i class="fa fa-youtube"></i></a><a href="https://www.facebook.com/escapegamefrance/?locale=fr_FR" target="_blank"><i class="fa fa-facebook"></i></a><a href="https://www.instagram.com/escapegamefrance" target="_blank"><i class="fa fa-instagram"></i></a></div>
+        </div>
+        <div id="logo">
+          <img src="./images/logo.png" alt="img_logo">
+        </div>
+        <img src="./images/menu-btn1.png" alt="menu_hamburger" class="menu_hamburger">
+      </div>
