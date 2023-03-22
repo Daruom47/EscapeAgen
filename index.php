c7c5d9729bf64fbc6a89a59486c038e6d1200046
StevenAEN <s.barbe@groupe-aen.info>
2023-03-22 10:43:19 +0100

    J'ai fais caca

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="./css/nav.css">
     <link href='https://fonts.googleapis.com/css?family=Recursive' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- HEY SEXY LADY<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"> -->
+    <!-- HEY SEXY LADYZZ<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"> -->
     <link rel="icon" type="image/png" href="./images/favicon/favicon-32x32.png" />
     <title>AGEN ESCAPE - ACCUEIL</title>
   </head>
