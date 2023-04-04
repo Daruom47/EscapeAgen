ec86acebac16431187bbf6a6dc761e045b4f978b
Daruom47 <carot4733@gmail.com>
2023-04-04 14:22:31 +0200

    correction User

--- templates/admin_head.php
+++ templates/admin_head.php
@@ -9,6 +9,7 @@
             <link rel="stylesheet" href="../css/styles.css" />
             <link rel="stylesheet" href="../css/admin.css?<?= time(); ?>" />
             <link rel="stylesheet" href="../css/admin_reservation.css">
+            <link rel="stylesheet" href="../css/admin_user.css">
             <!-- <link rel="stylesheet" href="../css/footer.css" /> -->
             <link rel="stylesheet" href="../css/nav.css">
             <link href='https://fonts.googleapis.com/css?family=Recursive' rel='stylesheet'>
